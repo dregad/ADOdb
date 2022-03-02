80de06ca0b7bd696d885e907eba518c0cbadc998
mnewnham <mark@newnhams.com>
2022-03-01 20:21:17 -0700

    Initial commit of data dictionary  for PDO drivers

--- adodb.inc.php
+++ adodb.inc.php
@@ -5938,14 +5938,29 @@ class ADORecordSet implements IteratorAggregate {
 
 		include_once(ADODB_DIR.'/adodb-lib.inc.php');
 		include_once(ADODB_DIR.'/adodb-datadict.inc.php');
-		$path = ADODB_DIR."/datadict/datadict-$drivername.inc.php";
+		
+		if ($conn->dsnType)
+		{
+			$cName = sprintf('%s_%s',$drivername,$conn->dsnType);
+			$dName = sprintf('datadict-%s-%s.inc.php',$drivername,$conn->dsnType);
+		}
+		else
+		{
+			$cName = sprintf('%s',$drivername);
+			$dName = sprintf('datadict-%s.inc.php',$drivername);
+		}
+		
+		$path = ADODB_DIR."/datadict/" . $dName;
 
 		if (!file_exists($path)) {
+
 			ADOConnection::outp("Dictionary driver '$path' not available");
 			return false;
+		
 		}
+
 		include_once($path);
-		$class = "ADODB2_$drivername";
+		$class = "ADODB2_$cName";
 		/** @var ADODB_DataDict $dict */
 		$dict = new $class();
 		$dict->dataProvider = $conn->dataProvider;
@@ -5955,7 +5970,7 @@ class ADORecordSet implements IteratorAggregate {
 		if (!empty($conn->_connectionID)) {
 			$dict->serverInfo = $conn->ServerInfo();
 		}
-
+		
 		return $dict;
 	}
 
